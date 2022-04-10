bf3410e31ce006a3f78e9e5e2f8353692ac67547
KristinaErdman <erdmakristina8@gmail.com>
2022-04-11 01:17:14 +0700

    fix urls for media and static

--- EventTestProject/urls.py
+++ EventTestProject/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,7 +7,8 @@ from events.urls import events_router
 from users.urls import users_router
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include(users_router.urls)),
-    path('events/', include(events_router.urls)),
-]
+                  path('admin/', admin.site.urls),
+                  path('users/', include(users_router.urls)),
+                  path('events/', include(events_router.urls)),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
+                                                                                         document_root=settings.STATIC_ROOT)
